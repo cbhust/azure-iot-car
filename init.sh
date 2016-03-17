e55f6be554c7001b2aeb73186a16ad536fb18569
Leonardo Graboski Veiga <leogveiga@gmail.com>
2016-03-17 12:15:25 +0000

    changes to the init script

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 sleep 2
 stty speed 9600 </dev/ttyLP2
 gpsd -n /dev/ttyLP2
-echo "starting HC-SR04 module"
 insmod /home/root/azure-iot-car/hcsr04.ko
 modprobe inv-mpu6050
+while : ; do
+        node /home/root/azure-iot-car/send_data_from_sensors.js
+	sleep 2
+done
